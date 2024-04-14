a4e6543feaf7fe5b91cf2c3ab0c7b8e5209fa7cc
EstefaniaGrowker <vsuykyasmbp@hotmail.com>
2024-04-14 13:03:18 -0300

    fix: deploy cors policy

--- backend/app.ts
+++ backend/app.ts
@@ -3,7 +3,7 @@ import router from './src/routes/index.ts'
 
 const app = express();
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
+    res.header('Access-Control-Allow-Origin',[ 'http://localhost:4000', 'https://full-stack-test-qrco.vercel.app/'])
     next()
 });
 
